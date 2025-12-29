93babb2abbed687a59d65a6a3767a30d3f4be5a1
whatnewcode <150623381+whatnewcode@users.noreply.github.com>
2025-12-29 11:12:33 -0600

    load data, split test train data.

--- src/python/machine-learning-pipeline-to-assess-credit-risk/test_train_data.py
+++ src/python/machine-learning-pipeline-to-assess-credit-risk/test_train_data.py
@@ -1,28 +1,43 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-try:
-    # load loan_data.csv into panda DF
+
+def csv_to_df(file_path: str) -> pd.DataFrame:
+    """Load a CSV file into a Pandas DataFrame.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The loaded DataFrame.
+    """
     try:
-        loan_data_df = pd.read_csv("loan_data.csv")
-        print(f"total rows from load_data df = {len(loan_data_df)} ")
-        # print(f"top 5 rows = {loan_data_df.head(5)}")
+        df = pd.read_csv(file_path)
+        return df
     except Exception as e:
-        print(f"Error while loading loan_data.csv into Pandas Data Frame - {e}")
+        print(f"Error loading CSV file at {file_path}: {e}")
         raise
 
-    seed = 10
-    # split input data into train and test
-    X_train, X_test, y_train, y_test = train_test_split(loan_data_df.drop("default", axis=1),
-                                                        loan_data_df["default"],
-                                                        test_size=0.2,
-                                                        train_size=0.8,
-                                                        random_state=seed)
+def split_test_train_data():
+    """"""
+    try:
+        # load loan_data.csv into panda DF
+        loan_data_df = csv_to_df("data/loan_data.csv")
 
-    print(f"train test shapes = {X_train.shape, X_test.shape}")
-    print(f"default rate train, test = {y_train.mean(), y_test.mean()}")
-    # print(f"top 1 record from train_data = {X_train.head(1)}")
-    # print(f"top 1 record from test_data = {X_test.head(1)}")
+        seed = 10
+        # split input data into train and test
+        X_train, X_test, y_train, y_test = train_test_split(loan_data_df.drop("default", axis=1),
+                                                            loan_data_df["default"],
+                                                            test_size=0.2,
+                                                            train_size=0.8,
+                                                            random_state=seed)
 
-except Exception as e:
-    print(f"Error: {e}")
+        print(f"train test shapes = {X_train.shape, X_test.shape}")
+        print(f"default rate train, test = {y_train.mean(), y_test.mean()}")
+        # print(f"top 1 record from train_data = {X_train.head(1)}")
+        # print(f"top 1 record from test_data = {X_test.head(1)}")
+
+        return X_train, X_test, y_train, y_test
+
+    except Exception as e:
+        print(f"Error: {e}")
